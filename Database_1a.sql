231ec48c01bfcf05f3d5c11f7c9f0290ad51e7a2
jasonlubrano <jalubrano1@gmail.com>
2017-11-01 16:45:30 -0600

    Updated for Milestone 3

--- Database_1a.sql
+++ Database_1a.sql
@@ -1,28 +1,33 @@
 #Started by Jason Lubrano
 #30 October 2017
 
-CREATE TABLE UserProfile
+CREATE TABLE User
 (
 id int PRIMARY KEY, #number automatically assigned to each user
 username varchar(20), NOT NULL #username
 password varchar(20), NOT NULL #password given to each user
+passworddigest varchar(20), NOT NULL #look back at it
 points int CHECK (points > 0), #always have positive points called a <<check constraint>> btw
-#gps location is what value? lat and longs?
 CONSTRAINT unique_user UNIQUE (username) #users cant have the same username
 );
 
-CREATE TABLE History
+CREATE TABLE Hints
 (
-#solved puzzles
-#gps location, lat and longs (?) GPSLOC int REFERENCES UserProfile(GPSLOC)
-		#^ orphan records that leaves bad data because we wont be there anymore(?)
-		#Can we just delete the challenge at that locaition all together for that user
-		#e.g. if a user solves the folsom stadium challenge, 
-		#it doesnt show up on their map with a checkmark noting they solved it
+id int PRIMARY KEY,
+locat_lat int NOT NULL,
+locat_long int NOT NULL,
+points int CHECK (points > 0),
+hint_name varchar(20) NOT NULL,
+#id of trea
+creator_user int REFERENCES User (id)
 )
 
 CREATE TABLE Treasure
 (
-
-
+id int PRIMARY KEY,
+locat_long int NOT NULL,
+locat_lat int NOT NULL,
+points int CHECK (points > 0),
+creator_user int REFERENCES user (id) NOT NULL,
+solved_user int REFERENCES user (id),
 )
