bf24172336fee2b213766ce5085fce3a35192ecd
Jason Lubrano <32202042+jasonlubrano@users.noreply.github.com>
2017-11-01 17:24:07 -0600

    Database mgt (#9)
    * fixed some syntax errors and changed variable names
    
    * fixed more syntax errors
    
    * fixed more syntax errors, ready for push
    
    * i think i fixed it

--- Database_1a.sql
+++ Database_1a.sql
@@ -4,9 +4,9 @@
 CREATE TABLE User
 (
 id int PRIMARY KEY, #number automatically assigned to each user
-username varchar(20), NOT NULL #username
-password varchar(20), NOT NULL #password given to each user
-passworddigest varchar(20), NOT NULL #look back at it
+username varchar(20) NOT NULL, #username
+password varchar(20) NOT NULL, #password given to each user
+passworddigest varchar(20) NOT NULL, #look back at it
 points int CHECK (points > 0), #always have positive points called a <<check constraint>> btw
 CONSTRAINT unique_user UNIQUE (username) #users cant have the same username
 );
@@ -17,17 +17,19 @@ id int PRIMARY KEY,
 locat_lat int NOT NULL,
 locat_long int NOT NULL,
 points int CHECK (points > 0),
-name varchar(20) NOT NULL,
+hintname varchar(20) NOT NULL,
 user_id_creator int REFERENCES User (id),
-treasure_id int REFERENCES Treasure (id)
-)
+treasure_id int REFERENCES Treasure (id),
+CONSTRAINT unique_hint UNIQUE (hintname)
+);
 
 CREATE TABLE Treasure
 (
 id int PRIMARY KEY,
+treasurename varchar(20) NOT NULL,
 locat_long int NOT NULL,
 locat_lat int NOT NULL,
 points int CHECK (points > 0),
-user_id_creator int REFERENCES user (id) NOT NULL,
-user_id_solver int REFERENCES user (id)
-)
+user_id_creator int REFERENCES User (id),
+CONSTRAINT unique_treasure UNIQUE (treasurename)
+);
