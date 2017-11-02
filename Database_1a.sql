32a9dc500595c5d1c4c87ed82938279b28646837
jasonlubrano <jalubrano1@gmail.com>
2017-11-01 18:09:33 -0600

    adding two files

--- Database_1a.sql
+++ Database_1a.sql
@@ -14,10 +14,10 @@ CONSTRAINT unique_user UNIQUE (username) #users cant have the same username
 CREATE TABLE Hints
 (
 id int PRIMARY KEY,
+hintname varchar(20) NOT NULL,
 locat_lat int NOT NULL,
 locat_long int NOT NULL,
 points int CHECK (points > 0),
-hintname varchar(20) NOT NULL,
 user_id_creator int REFERENCES User (id),
 treasure_id int REFERENCES Treasure (id),
 CONSTRAINT unique_hint UNIQUE (hintname)
@@ -27,8 +27,8 @@ CREATE TABLE Treasure
 (
 id int PRIMARY KEY,
 treasurename varchar(20) NOT NULL,
-locat_long int NOT NULL,
 locat_lat int NOT NULL,
+locat_long int NOT NULL,
 points int CHECK (points > 0),
 user_id_creator int REFERENCES User (id),
 CONSTRAINT unique_treasure UNIQUE (treasurename)
