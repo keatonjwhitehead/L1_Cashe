0bb3966a4ad7ae6a26fca6423cc4702b915f37a6
Suyog Soti <suyog.soti@gmail.com>
2017-11-19 16:02:53 -0700

    fixed syntax for the sql

--- Database_1a.sql
+++ Database_1a.sql
@@ -4,7 +4,7 @@
 CREATE TABLE User
 (
 id int PRIMARY KEY, #number automatically assigned to each user
-username varchar(20) NO	T NULL, #username
+username varchar(20) NOT NULL, #username
 password varchar(20) NOT NULL, #password given to each user
 points int CHECK (points > 0), #always have positive points called a <<check constraint>> btw
 CONSTRAINT unique_user UNIQUE (username) #users cant have the same username
@@ -19,7 +19,7 @@ locat_long int NOT NULL,
 points int CHECK (points > 0),
 user_id_creator int REFERENCES User (id),
 treasure_id int REFERENCES Treasure (id),
-CONSTRAINT unique_hint UNIQUE (hintname)	
+CONSTRAINT unique_hint UNIQUE (hintname)
 );
 
 CREATE TABLE Treasure
