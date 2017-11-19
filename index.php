b39c86cef4713b9622e53b617ca0fa66d74ac765
lindapalacios <linda.palaciosrivera@colorado.edu>
2017-11-19 01:52:28 -0700

    Development (#14)
    * initial commit
    
    * fixed some spacing
    
    * some more spacing
    
    * added data app
    
    * added static pages and made data work
    
    * Add files via upload
    
    * Mongo integration (#7)
    
    * First database roughdraft
    
    * Updated for Milestone 3
    
    * fixed some syntax errors and changed variable names (#8)
    
    * Database mgt (#9)
    
    * fixed some syntax errors and changed variable names
    
    * fixed more syntax errors
    
    * fixed more syntax errors, ready for push
    
    * i think i fixed it
    
    * Database mgt (#10)
    
    * fixed some syntax errors and changed variable names
    
    * fixed more syntax errors
    
    * fixed more syntax errors, ready for push
    
    * i think i fixed it
    
    * adding two files
    
    * adding the access database file
    
    * adding the pdf
    
    * Front end (#11)
    
    * Add files via upload
    
    work in progress. footerblurb is not center and graphics need to be added
    
    * Add files via upload
    
    work in progress. Sign up box is too big
    
    * Update signup.html
    
    Added Bootstrap and changed style for Sign Up page
    
    * Update splash.html
    
    Added Bootstrap and changed style for Splash page
    
    * Update signup.html
    
    I added "Repeat Password" prompt, added a Cancel button, and changed background color for Submit and Cancel buttons
    
    * deleted the django stuff
    
    * integrated front end so far
    
    * Database mgt (#12)
    
    * fixed some syntax errors and changed variable names
    
    * fixed more syntax errors
    
    * fixed more syntax errors, ready for push
    
    * i think i fixed it
    
    * adding two files
    
    * adding the access database file
    
    * adding the pdf
    
    * added history table
    
    * took out user.passworddigest and fixed syntax errors.

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+
+
+include "header.php";
+
+myHeader("Home");
+
+?>
+
+
+	<!-- Content -->
+		<div class="row content">
+			<div class="col-sm-2 sidenav">
+				<p><a href="#" style="color:blue">Rules</a></p>
+				<p><a href="#" style="color:blue">Feedback</a></p>
+			</div>
+			<div class="col-sm-8"> 
+				<center>
+					<h1  style="margin-bottom:8%;">How does it work?</h1>
+				</center>
+				<div class="row">
+					<div class="col-sm-6 col-sm-offset-6" style="margin-bottom:8%;">
+						<h2>1. Create a free account</h2>
+						<p>Solve puzzles throught out Boulder, you can play by yourself or with a friend</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-6"  style="margin-bottom:8%;">
+						<h2>2. Arrive at GPS location</h2>
+						<p>Upon arriving at a particular location, information regarding the next marker will be provided</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-6 col-sm-offset-6"  style="margin-bottom:8%;">
+						<h2>3. Provide feedback</h2>
+						<p>Once you completed a challenge, share your experience. Your feedback will be used to improve website</p>
+					</div>
+				</div>
+				<hr>
+				<div class="row panel panel-default"  style="margin-top:10%;">
+					<div class="col-sm-5 col-sm-offset-4">
+						<center>
+							<div class="panel-body">
+								<h1>Vision Statement</h1>
+								<p style="font-size:17px">To create a social geocaching application which inspires exploration and collaboration among CU students</p>
+							</div>
+						</center>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+
+
+<?php
+
+include "footer.php";
