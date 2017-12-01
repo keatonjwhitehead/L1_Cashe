670b4062b7e913053bfa0a0ab491bbfeaf3d17a9
lindapalacios <linda.palaciosrivera@colorado.edu>
2017-12-01 14:14:49 -0700

    Update treasureFunctions.php
    I made the changes to the first part of the function. I will make the changes to the other part later tonight

--- treasure/treasureFunctions.php
+++ treasure/treasureFunctions.php
@@ -17,25 +17,71 @@ function treasureForm($row)
 	 */
 	if ($row) {
 ?>
-		<form action="/user.php" method="post">
-			<div class="col-sm-4 col-sm-offset-4">
-				<label for="treasure-id-i">Treasure ID:</label>
-				<input type="text" class="form-control" value="<?php echo $row['id'];?>" readonly name="treasure-id-i"  style="margin-bottom:4%">
-				<label for="treasure-treasurename-s">Treasure Name:</label>
-				<input type="text" class="form-control" value="<?php echo $row['treasurename'];?>" name="treasure-treasurename-s"  style="margin-bottom:4%">
-				<label for="treasure-locat_lat-d">Latitude (Please do not edit this unless you are sure of what you are doing!):</label>
-				<input type="text" class="form-control" value="<?php echo $row['locat_lat'];?>" name="treasure-locat_lat-d"  style="margin-bottom:4%">
-				<label for="treasure-locat_long-d">Longitude (Please do not edit this unless you are sure of what you are doing!):</label>
-				<input type="text" class="form-control" value="<?php echo $row['locat_long'];?>" name="treasure-locat_long-d"  style="margin-bottom:4%">
-				<label for="treasure-points-i">Rewards:</label>
-				<input type="text" class="form-control" value="<?php echo $row['points'];?>" name="treasure-points-i"  style="margin-bottom:4%">
-				<label for="treasure-solver_username-s">Solver:</label>
-				<input type="text" class="form-control" placeholder="No one has solved the Treasure yet..." value="<?php echo $row['solver_username'];?>" style="margin-bottom:4%" readonly>
-				<center>
-					<button type="submit" class="btn btn-default" style="background:#69f0ae"><strong>Update</strong></button>
-				</center>
+	<form action="/user.php" method="post">
+		<div class="col-sm-6">
+			<div class="panel panel-default" style="margin:10%">
+				<div class="panel-heading">
+					<label for="treasure-id-i"><h1 class="panel-title">Treasure ID:</h1></label>
+				</div>
+				<div class="panel-body">
+					<input type="text" class="form-control" value="<?php echo $row['id'];?>" readonly name="treasure-id-i"  style="margin-bottom:4%">
+				</div>
 			</div>
-		</form>
+		</div>
+		<div class="col-sm-6">
+			<div class="panel panel-default" style="margin:10%">
+				<div class="panel-heading">
+					<label for="treasure-treasurename-s"><h1 class="panel-title">Treasure Name:</h1></label>
+				</div>
+				<div class="panel-body">
+					<input type="text" class="form-control" value="<?php echo $row['treasurename'];?>" name="treasure-treasurename-s"  style="margin-bottom:4%">
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-6">
+			<div class="panel panel-default" style="margin:10%">
+				<div class="panel-heading">
+					<label for="treasure-locat_lat-d"><h1 class="panel-title">Latitude (Please do not edit this unless you are sure of what you are doing!):</h1></label>
+				</div>
+				<div class="panel-body">
+					<input type="text" class="form-control" value="<?php echo $row['locat_lat'];?>" name="treasure-locat_lat-d"  style="margin-bottom:4%">
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-6">
+			<div class="panel panel-default" style="margin:10%">
+				<div class="panel-heading">
+					<label for="treasure-locat_long-d"><h1 class="panel-title">Longitude (Please do not edit this unless you are sure of what you are doing!):</h1></label>
+				</div>
+				<div class="panel-body">
+					<input type="text" class="form-control" value="<?php echo $row['locat_long'];?>" name="treasure-locat_long-d"  style="margin-bottom:4%">
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-12">
+			<div class="panel panel-default" style="margin:5%">
+				<div class="panel-heading">
+					<label for="treasure-points-i"><h1 class="panel-title">Rewards:</h1></label>
+				</div>
+				<div class="panel-body">
+					<input type="text" class="form-control" value="<?php echo $row['points'];?>" name="treasure-points-i"  style="margin-bottom:4%">
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-12">
+			<div class="panel panel-default" style="margin:5%">
+				<div class="panel-heading">
+					<label for="treasure-solver_username-s"><h1 class="panel-title">Solver:</h1></label>
+				</div>
+				<div class="panel-body">
+					<input type="text" class="form-control" placeholder="No one has solved the Treasure yet..." value="<?php echo $row['solver_username'];?>" style="margin-bottom:4%" readonly>
+				</div>
+			</div>
+			<center style="margin-bottom:5%">
+				<button type="submit" class="btn btn-default" style="background:#69f0ae"><strong>Update</strong></button>
+			</center>
+		</div>	
+	</form>
 <?php
 	} else {
 ?>
