dbbeb0455421f7d4645c7c7be06d01700f84fbc8
Suyog Soti <suyog.soti@gmail.com>
2017-12-04 22:42:23 -0700

    ajax returns row of correct hints

--- verify.php
+++ verify.php
@@ -1,20 +1,53 @@
 <?php
 
 include "header.php";
-
+include 'hint/hintFunctions.php';
 $lat = $_POST['lat'];
 $long = $_POST['long'];
 
-$hint = $link->query("Select * from Hint where locat_lat=$lat AND locat_long=$long");
-$treasure = $link->query("Select * from Treasure where locat_lat=$lat AND locat_long=$long");
-$history = $link->query("Select * from History where solver_username='$username'");
-/* echo var_dump($hint); */
-/* echo var_dump($treasure); */
-/* echo var_dump($history); */
+$upper_lat = $lat + 0.15;
+$lower_lat = $lat - 0.15;
+$upper_long = $long + 0.15;
+$lower_long = $long - 0.15;
+$username = currentUser();
+$hint = $link->query("Select * from Hint where locat_lat between $lower_lat AND $upper_lat AND locat_long between $lower_long AND $upper_long");
+$treasure = $link->query("Select * from Treasure where locat_lat between $lower_lat AND $upper_lat AND locat_long between $lower_long AND $upper_long");
+
+/* $hint = $link->query("Select * from Hint"); */
+/* $treasure = $link->query("Select * from Treasure"); */
+/* $history = $link->query("Select * from History"); */
+
+// $displayTreasure = array();
+// if ($treasure->num_rows > 0) {
+// 	while ($row = $treasure->fetch_assoc()) {
+// 		echo var_dump($row);
+// 		$id = $row['id'];
+// 		$hints = $link->query("Select hint_id from History where solver_username=$username AND treasure_id=$id");
+// 		echo $hints->num_rows;
+// 	}
+// }
 
 $displayHint = array();
 if ($hint->num_rows > 0) {
 	while ($row = $hint->fetch_assoc()) {
-		echo var_dump($row);
+		$id = $row['id'];
+		$ids = $link->query("Select id from Hint where next_hint=$id");
+		$id = NULL;
+		if ($ids->num_rows > 0) {
+			// do it here
+			while ($id = $ids->fetch_assoc()) {
+				$myID = $id['id'];
+				$num = $link->query("Select hint_id from History where hint_id=$myID AND solver_username='$username'");
+				if ($num->num_rows > 0) {
+					$id = true;
+					break;
+				}
+			}
+		} else {
+			$id = true;
+		}
+		if ($id) {
+			hintForm($row);
+		}
 	}
 }
