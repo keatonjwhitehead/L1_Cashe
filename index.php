9f681024405228badade13f2b2bfc904d425e2c3
lindapalacios <linda.palaciosrivera@colorado.edu>
2017-12-11 18:23:56 -0700

    Update index.php
    Added images: Map-01.svg, MagnifyingGlass-01.svg, and Prize-01.svg and changed the instruction to add Blake's work. Also, took out "Rules" and "Feedback" links from right container

--- index.php
+++ index.php
@@ -9,43 +9,45 @@ myHeader("Home");
 
 
 	<!-- Content -->
-		<div class="row content">
-			<div class="col-sm-2 sidenav">
-				<p><a href="#" style="color:blue">Rules</a></p>
-				<p><a href="#" style="color:blue">Feedback</a></p>
+		<div class="col-sm-8 col-sm-offset-2" style="background:rgba(0,188,212,0.1)"> 
+			<h1 class="text-center" style="font-size:600%; margin-bottom:8%;">How does it work?</h1>	
+			<div class="row" style="margin-bottom:5%;">
+				<div class="col-sm-6">
+					<object type="image/svg+xml" data="Map-01.svg"></object>
+				</div>
+				<div class="col-sm-6">
+					<h2>EXPLORE:</h2>
+					<p>Look for treasure in and around CU's beautiful campus. From the depths
+					of the engineering center to the crest of the hill, Cache Me if You Can's
+					riddles will require extensive exploration of the city of Boulder. Get your
+					map ready and shoes tied... the hunt is about to begin.</p>
+				</div>
 			</div>
-			<div class="col-sm-8"> 
-				<center>
-					<h1  style="margin-bottom:8%;">How does it work?</h1>
-				</center>
-				<div class="row">
-					<div class="col-sm-6 col-sm-offset-6" style="margin-bottom:8%;">
-						<h2>1. Create a free account</h2>
-						<p>Solve puzzles throught out Boulder, you can play by yourself or with a friend</p>
-					</div>
+				
+			<div class="row" style="margin-bottom:5%;">
+				<div class="col-sm-6">
+					<object type="image/svg+xml" data="MagnifyingGlass-01.svg"></object>
 				</div>
-				<div class="row">
-					<div class="col-sm-6"  style="margin-bottom:8%;">
-						<h2>2. Arrive at GPS location</h2>
-						<p>Upon arriving at a particular location, information regarding the next marker will be provided</p>
-					</div>
+				<div class="col-sm-6">
+					<h2>SOLVE:</h2>
+					<p>Test your wit by solving complex riddles. Develop your skills by overcoming
+					difficult challenges. Only those with courage begin a Cache Me if You Can hunt, and
+					only those with determination complete them. These treasure quests are not for the faint
+					of heart. Are you up to the task? </p>
 				</div>
-				<div class="row">
-					<div class="col-sm-6 col-sm-offset-6"  style="margin-bottom:8%;">
-						<h2>3. Provide feedback</h2>
-						<p>Once you completed a challenge, share your experience. Your feedback will be used to improve website</p>
-					</div>
+			</div>
+				
+			<div class="row" style="margin-bottom:10%;">
+				<div class="col-sm-6">
+					<object type="image/svg+xml" data="Prize-01.svg"></object>
 				</div>
-				<hr>
-				<div class="row panel panel-default"  style="margin-top:10%;">
-					<div class="col-sm-5 col-sm-offset-4">
-						<center>
-							<div class="panel-body">
-								<h1>Vision Statement</h1>
-								<p style="font-size:17px">To create a social geocaching application which inspires exploration and collaboration among CU students</p>
-							</div>
-						</center>
-					</div>
+				<div class="col-sm-6">
+					<h2>WIN:</h2>
+					<p>Win prizes by being the first to complete a treasure hunt. For those
+					who finish later there is no cash prize; however, you earn the biggest reward
+					of them all: personal satisfaction in completing an arbitrary challenge 
+					established by your peers as a way of keeping you busy. Isn't that what college
+					is all about?</p>
 				</div>
 			</div>
 		</div>
